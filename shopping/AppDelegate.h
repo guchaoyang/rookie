f6e73b67372ee9984193060552c6af27a378bacc
谷朝阳 <GCY@gcy.local>
2017-12-07 17:03:12 +0800

    Initial Commit

---
+++ shopping/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  shopping
+//
+//  Created by 谷朝阳 on 2017/12/7.
+//  Copyright © 2017年 GCY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ shopping/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  shopping
+//
+//  Created by 谷朝阳 on 2017/12/7.
+//  Copyright © 2017年 GCY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
